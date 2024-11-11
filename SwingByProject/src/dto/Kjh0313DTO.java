967fe576f42ceeeed52eb8bbcfeb8676768754df
kimjaehan06 <onlykjh06@gmail.com>
2024-11-11 16:07:53 +0900

    김재한 dao dto Jpanel

---
+++ SwingByProject/src/dto/Kjh0313DTO.java
@@ -0,0 +1,21 @@
+package dto;
+
+public class Kjh0313DTO {
+	
+	public final int empno;
+	public final String ename;
+	public final String job;
+	public final int deptno;
+	public final String dname;
+	public final String loc;
+	
+	public Kjh0313DTO(int empno, String ename, String job, int deptno, String dname, String loc) {
+		this.empno = empno;
+		this.ename = ename;
+		this.job= job;
+		this.deptno = deptno;
+		this.dname = dname;
+		this.loc = loc;
+	}
+	
+}
