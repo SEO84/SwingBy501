8892420e86ec59b5b0dd52fa7400c0298648b0fc
pvpanime <nemisprint@gmail.com>
2024-11-11 12:18:12 +0900

    push template project

---
+++ SwingByProject/src/MainApp.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class MainApp extends JFrame {
+	
+	
+	public MainApp() {
+		this.setSize(500, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainApp();
+	}
+
+}
